5aece2a31b1aa1f7ee947933e2c30a24d197331c
abbashaid <60725620+abbashaid@users.noreply.github.com>
2021-05-23 11:00:51 +0500

    Created function Expand Clique

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+from random import random
 import numpy as np
+import random
 
 # print(np.logical_xor([1, 1, 0, 0], [1, 0, 1, 0]))
 # print(1 & 0)
@@ -19,3 +21,27 @@ import numpy as np
 #     if L[c] == '1':
 #         print(c//2)
 #     c+=2
+
+def ExpandClique(Chrom,G):
+    # Chrom is a vector with binary values
+
+    nodes = []
+    for i in range(len(Chrom)):
+        if Chrom[i] == 1:
+            nodes.append(i+1)
+
+    k = nodes.pop(random.randrange(len(nodes)))
+    CliqueV = [] 
+    CliqueV.append(k)
+
+    for n in nodes:
+        if all( if (n in G[v]) for v in CliqueV): # If n is in the neighbourhood of all nodes in the clique
+            CliqueV.append(n)
+                
+
+    # Create New Chromosome of updated clique       
+    NewChrom = [0] * len(Chrom)
+    for v in CliqueV:
+        NewChrom[v-1] = 1
+    
+
